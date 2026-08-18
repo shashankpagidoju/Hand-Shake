0af928634b29d3f5bb83446727cedc9aafcc3981
krishcdev <krish1507choudary@gmail.com>
2026-08-18 22:12:53 +0530

    partical effect added to ball

--- Assets/Scripts/Main Scripts/Death.cs
+++ Assets/Scripts/Main Scripts/Death.cs
@@ -3,12 +3,42 @@ using UnityEngine;
 public class Death : MonoBehaviour
 {
     public GameManager gameManager;
+    public GameObject ballBreakEffect;
+
+    private bool hasDied = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
+        if (hasDied)
+            return;
+
         if (other.CompareTag("Death"))
         {
+            hasDied = true;
+
+            // Spawn shatter effect
+            GameObject effect = Instantiate(
+                ballBreakEffect,
+                transform.position,
+                Quaternion.identity
+            );
+
+            // Start particles
+            ParticleSystem particles = effect.GetComponent<ParticleSystem>();
+
+            if (particles != null)
+            {
+                particles.Play();
+            }
+
+            // Hide ball
+            gameObject.SetActive(false);
+
+            // Game Over
             gameManager.GameOver();
+
+            // Destroy effect after particles finish
+            Destroy(effect, 2f);
         }
     }
 }
